2a904339dbe2c3c35430ef0e50f55ad6e40f1d03
AmineYoussef <amine.youssef@xevlabs.com>
2022-03-18 09:53:48 +0100

    Update ask.model.ts

--- libs/trustoo-utils/src/lib/models/ask.model.ts
+++ libs/trustoo-utils/src/lib/models/ask.model.ts
@@ -1,6 +1,6 @@
-import { AddressModel } from './address.model';
-import { ClientModel } from './client.model';
-import { VendorModel } from './vendor.model';
+import { AddressModel } from './address.model'
+import { ClientModel } from './client.model'
+import { VendorModel } from './vendor.model'
 
 export interface AskModel {
     id: string,
